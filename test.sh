f73749a3cb03f4b5abb4b68a30e3da7776026318
Shivayogi Kamat <yogisdmec007@gmail.com>
2021-03-06 22:16:19 +0530

    code update

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-println("HELLO")
+geturl1 = new URL('https://calendarific.com/api/v2/holidays?&api_key=d20d05ccb411d9ce3b56b654971e17a29b0aa1ed&country=IN&year=2021').openConnection()
+getRC1 = geturl1.getResponseCode();
