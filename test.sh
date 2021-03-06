2a8d2b8dbc8468ea88b53410dbc36d53bdb4c662
Shivayogi Kamat <yogisdmec007@gmail.com>
2021-03-06 22:21:36 +0530

    code update

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 echo "Hello world"
-geturl1=new URL('https://calendarific.com/api/v2/holidays?&api_key=d20d05ccb411d9ce3b56b654971e17a29b0aa1ed&country=IN&year=2021').openConnection()
-getRC1=geturl1.getResponseCode();
+def get = new URL("https://calendarific.com/api/v2/holidays?&api_key=d20d05ccb411d9ce3b56b654971e17a29b0aa1ed&country=IN&year=2021").openConnection();
+def getRC = get.getResponseCode();
+println(getRC);
+if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+}
